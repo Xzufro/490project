3458488aada040384bae58c4d2d8088de34098e0
ARTURO-RM-101 <aroman694@csu.fullerton.edu>
2026-04-30 17:09:16 -0700

    Fixed the program to be executable and have the webpage open

---
+++ app/components/GamePage.tsx
@@ -0,0 +1,10 @@
+import './styles.css'
+
+export default function GamePage() {
+    return (
+        <div>
+            <h1> Game Page </h1>
+            <p> here's some content for the game page!! </p>
+        </div>
+    )
+}
